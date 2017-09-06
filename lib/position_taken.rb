c945e8e35e33ec6aa538b3a2a365a565b1d16571
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 09:32:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board,index)
   if board[index]==" " || board[index]=="" || board[index]==nil
       false
-  elsif board[index]!=" " || board[index]!="" || board[index]!="nil"
+  else
       true
   end
 end
