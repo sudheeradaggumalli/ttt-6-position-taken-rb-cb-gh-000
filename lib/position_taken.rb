10dd49e326f8516c4986cbe97f2a8e8bd162aaa5
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 09:29:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  board[index]==" " || board[index]=="" || board[index]=="nil"
-  board[index]!=" " || board[index]!="" || board[index]!="nil"
+  if board[index]==" " || board[index]=="" || board[index]=="nil"
+      false
+  elsif board[index]!=" " || board[index]!="" || board[index]!="nil"
+      true
 end
